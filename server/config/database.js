1645e3b4555579758c8bf012347cc98e6f0403f7
Mehran <alonephoenix90@gmail.com>
2025-10-17 15:23:40 +0330

    Init commit

--- server/config/database.js
+++
@@ -1,7 +0,0 @@
-const { runHtml2CanvasScript } = require('../data/util/fileDelete');
-
-const connectDatabase = () => {
-    runHtml2CanvasScript();
-}
-
-module.exports = connectDatabase;
--- src/pages/NoPage.jsx
+++
@@ -1,7 +0,0 @@
-import Img404 from "/src/assets/img/404.png";
-
-const NoPage = () => {
-  return <img src={Img404} className="w-100" />;
-};
-
-export default NoPage;
